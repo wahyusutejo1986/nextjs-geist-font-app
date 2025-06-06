3302c39eb19b48b8b7b3a040af0e4f6090690549
Wahyu Sutejo <wahyusutejo1986@gmail.com>
2025-06-06 20:33:46 +0700

    Add .next/static/chunks/src_app_page_tsx_64f27c9b._.js 1749216826226

---
+++ .next/static/chunks/src_app_page_tsx_64f27c9b._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a8a0ba4c._.js",
+  "static/chunks/src_b9f4947b._.js"
+],
+    source: "dynamic"
+});
